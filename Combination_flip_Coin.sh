b58a89af29ac8e52fa3b3d55ddb5c454795bc23a
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-07-10 23:40:54 +0530

    [Prasanth] Created Singles and Calculated their percentage

--- Combination_flip_Coin.sh
+++ Combination_flip_Coin.sh
@@ -1,14 +1,40 @@
-#!/bin/bash
+#!/usr/local/bin/bash
  
 #CONSTANTS
 IS_HEAD=1
 IS_TAILS=0
 
-result=$((RANDOM%2))
+read -p "Enter a Number to limit the Simulation : " input 
 
-if [[ $result -eq $IS_HEAD ]]
-then
-	echo HEAD
-else
-	echo TAIL
-fi
+function singletGeneration(){
+
+#LOCAL_VARIABLES
+count=0
+HEADcount=0
+TAILcount=0
+echo THE GENERATED SINGLE VALUES ARE:
+
+while (($count<$input))
+do
+   result=$((RANDOM%2))
+   echo $result
+   if [[ $result -eq $IS_HEAD ]]
+   then
+      CounterDictionary[H]=$((++HEADcount))
+   else
+      CounterDictionary[T]=$((++TAILcount))
+   fi
+   ((count++))
+done
+
+
+Hpercentage=$(echo "scale = 1; $HEADcount/$input*100" | bc)
+
+Tpercentage=$(echo "scale = 1; $TAILcount/$input*100" | bc)
+
+echo PERCENTAGE OF VALUES AS FOLLOWS :
+echo H = $Hpercentage%
+echo T = $Tpercentage%
+}
+
+singletGeneration
